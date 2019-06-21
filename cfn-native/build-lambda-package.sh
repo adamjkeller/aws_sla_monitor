ea2e01ab380ffc45679e5abedb4ca1d3df95cad9
Joe Taylor <joeta@amazon.com>
2019-06-20 17:20:39 -0700

    Corrected python version issue in build (python 3.6). Corrected environment variable name in main.py

--- cfn-native/build-lambda-package.sh
+++ cfn-native/build-lambda-package.sh
@@ -1,8 +1,8 @@
-virtualenv v-env
+virtualenv --python=/usr/bin/python3.6 v-env
 source v-env/bin/activate
 pip install -r ../requirements.txt
 deactivate
-cd v-env/lib/python2.7/site-packages/
+cd v-env/lib/python3.6/site-packages/
 zip -r9 ../../../../slamonitor-lambda-code.zip .
 cd ../../../../
 zip -g -j slamonitor-lambda-code.zip ../src/*
