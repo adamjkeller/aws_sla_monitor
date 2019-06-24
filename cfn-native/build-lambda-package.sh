11cbb209350482601e0739beee80cbca902dd6f6
Keller <keladam@f01898543b21.ant.amazon.com>
2019-06-24 11:36:55 -0700

    Final updates with master

--- cfn-native/build-lambda-package.sh
+++ cfn-native/build-lambda-package.sh
@@ -1,8 +1,17 @@
-virtualenv --python=/usr/bin/python3.6 v-env
-source v-env/bin/activate
+#!/usr/bin/env bash
+
+python3=$(which python3)
+
+# Setup and use virtualenv to isolate packages to build
+virtualenv --python=python3 venv
+source venv/bin/activate
+
+# Install packages and exit virtual environment
 pip install -r ../requirements.txt
 deactivate
-cd v-env/lib/python3.6/site-packages/
+
+# Package and compress python dependencies and lambda code
+cd venv/lib/python3*/site-packages/
 zip -r9 ../../../../slamonitor-lambda-code.zip .
 cd ../../../../
 zip -g -j slamonitor-lambda-code.zip ../src/*
